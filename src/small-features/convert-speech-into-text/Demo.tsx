130cd9dff8e5d03a670a76fff24ef364404ea819
MinhLeeManabie <leminh.nguyen@manabie.com>
2022-11-26 16:11:35 +0700

    feat: complete convert speech into text

--- src/small-features/convert-speech-into-text/Demo.tsx
+++ src/small-features/convert-speech-into-text/Demo.tsx
@@ -1,65 +1,19 @@
 import { Helmet } from "react-helmet-async";
 import PageTitleWrapper from "src/components/PageTitleWrapper";
-import { Container, Grid } from "@mui/material";
-import Footer from "src/components/Footer";
+import { Container, Grid, ListItem, ListItemText, Switch } from "@mui/material";
 import PageHeader from "src/content/dashboards/Crypto/PageHeader";
 import useSpeechRecognition from "./useSpeechRecognition";
-import { useState, useEffect } from "react";
-
-const SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition;
-const mic = new SpeechRecognition();
-
-mic.continuous = true;
-mic.interimResults = true;
-mic.lang = "en-US";
 
 function ManagementUserSettings() {
-    // const { onStartMic, onStopMic } = useSpeechRecognition();
+    const { onStartMic, onStopMic, getContent } = useSpeechRecognition();
 
-    const [isListening, setIsListening] = useState(false);
-    const [note, setNote] = useState(null);
-    const [savedNotes, setSavedNotes] = useState([]);
-
-    useEffect(() => {
-        handleListen();
-    }, [isListening]);
-
-    console.log(mic)
-
-    const handleListen = () => {
-        if (isListening) {
-            mic.start();
-            mic.onend = () => {
-                console.log("continue..");
-                mic.start();
-            };
+    const onChangeSwitchMic = (e) => {
+        if (e.target.checked) {
+            onStartMic();
         } else {
-            mic.stop();
-            mic.onend = () => {
-                console.log("Stopped Mic on Click");
-            };
+            onStopMic();
         }
-        mic.onstart = () => {
-            console.log("Mics on");
-        };
-
-        mic.onresult = (event) => {
-            const transcript = Array.from(event.results)
-                .map((result) => result[0])
-                .map((result) => result.transcript)
-                .join("");
-            console.log(transcript);
-            setNote(transcript);
-            mic.onerror = (event) => {
-                console.log(event.error);
-            };
-        };
-    };
-
-    const handleSaveNote = () => {
-        setSavedNotes([...savedNotes, note]);
-        setNote("");
-    };
+    }
 
     return (
         <>
@@ -68,7 +22,6 @@ function ManagementUserSettings() {
             </Helmet>
             <PageTitleWrapper>
                 <PageHeader />
-                <h2>Speech Recognition</h2>
             </PageTitleWrapper>
             <Container maxWidth="lg">
                 <Grid
@@ -78,18 +31,20 @@ function ManagementUserSettings() {
                     alignItems="stretch"
                     spacing={3}
                 >
-                    <>
-                        <h1>Voice Notes</h1>
-                        <div className="container">
-                            <div className="box">
-                                {/* <button onClick={onStartMic}>Start</button>
-                                <button onClick={onStopMic}>Stop</button> */}
-                            </div>
-                        </div>
-                    </>
+                    <ListItem sx={{ p: 3 }}>
+                        <ListItemText
+                            primaryTypographyProps={{ variant: "h5", gutterBottom: true }}
+                            secondaryTypographyProps={{
+                                variant: "subtitle2",
+                                lineHeight: 1,
+                            }}
+                            primary="Convert speech into text by Window Speech Recognition"
+                            secondary={getContent() || "Please open mic and talk anything ..."}
+                        />
+                        <Switch color="primary" onChange={onChangeSwitchMic} />
+                    </ListItem>
                 </Grid>
             </Container>
-            <Footer />
         </>
     );
 }
